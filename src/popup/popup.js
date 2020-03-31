83df1149b32ceb2f2d20a4a956c3167e8a592029
Albert Luta <luta.albert100@gmail.com>
2020-04-01 00:10:22 +0300

    Changed the logic in which the message/error is transmitted

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -2,6 +2,30 @@ const toggleScrollAndScrape = document.querySelector('button[name="toggle-scroll
 
 toggleScrollAndScrape.addEventListener('click', () => {
 	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-		chrome.tabs.sendMessage(tabs[0].id, { toggleScrollAndScrape: true });
+		if (tabs[0].url.startsWith('https://www.facebook.com/groups')) {
+			if (tabs[0].url.includes('sorting_setting=CHRONOLOGICAL')) {
+				chrome.tabs.sendMessage(tabs[0].id, { toggleScrollAndScrape: true });
+			} else {
+				chrome.tabs.update(
+					tabs[0].id,
+					{
+						active: true,
+						url: `${tabs[0].url}${
+							tabs[0].url.includes('?') ? '&' : '?'
+						}sorting_setting=CHRONOLOGICAL`
+					},
+					(tab) => {
+						const listener = chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+							if (tabId === tab.id && changeInfo.status === 'complete') {
+								chrome.tabs.onUpdated.removeListener(listener);
+								chrome.tabs.sendMessage(tabId, { toggleScrollAndScrape: true });
+							}
+						});
+					}
+				);
+			}
+		} else {
+			chrome.tabs.sendMessage(tabs[0].id, { error: 'This is not a facebook group' });
+		}
 	});
 });
