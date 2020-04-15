42df9979d5864609889ab55e024024f065dab0f2
Albert Luta <luta.albert100@gmail.com>
2020-04-15 20:11:58 +0300

    Back to normal, bby

--- src/background/background.js
+++ src/background/background.js
@@ -1,30 +1,3 @@
-// chrome.webRequest.onHeadersReceived.addListener(
-// 	(info) => {
-// 		const headers = info.responseHeaders;
-// 		for (let i = headers.length - 1; i >= 0; --i) {
-// 			const header = headers[i].name.toLowerCase();
-// 			if (header === 'x-frame-options' || header === 'frame-options') {
-// 				headers.splice(i, 1); // Remove header
-// 			}
-// 		}
-// 		return { responseHeaders: headers };
-// 	},
-// 	{
-// 		urls: ['https://www.facebook.com/groups/*'],
-// 		types: ['sub_frame'],
-// 	},
-// 	['blocking', 'responseHeaders']
-// );
-
-// ----------------------------
-
-// const iframe = document.querySelector('iframe');
-// iframe.addEventListener('load', (e) => {
-// 	// Do stuff
-// });
-
-// ----------------------------
-
 /**
  * The interval for the alarms(in minutes)
  */
